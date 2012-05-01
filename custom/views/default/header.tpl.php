81c71ef24a09e6bb7e2054fb6a96882ab6ae924d
Fabien Cazenave <fabien@cazenave.cc>
2012-05-01 20:06:00 +0200

    rewrite: /archives

--- custom/views/default/header.tpl.php
+++ custom/views/default/header.tpl.php
@@ -5,7 +5,7 @@
       <li><a href="./">Accueil</a></li>
       <li><a href="./contrib">Contribuer</a></li>
       <li><a href="./manifesto">À propos</a></li>
-      <li><a href="./?type=archive">Archives</a></li>
+      <li><a href="./archives">Archives</a></li>
     </ul>
   </nav>
 </header>
