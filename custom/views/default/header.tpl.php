1eba154536b6a63c28e0c7807e830ebcb90de38a
pascalc <pascal.chevrel@free.fr>
2012-11-20 16:12:44 +0100

    ajout d'un lien vers les forums dans le menu principal

--- custom/views/default/header.tpl.php
+++ custom/views/default/header.tpl.php
@@ -3,6 +3,7 @@
   <nav>
     <ul>
       <li><a href="./">Accueil</a></li>
+      <li><a href="http://forums.mozfr.org/">Forums</a></li>
       <li><a href="./participer">Participer</a></li>
       <li><a href="./manifesto">À propos</a></li>
       <li><a href="./archives">Archives</a></li>
